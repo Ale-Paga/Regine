b49b8a4f587b7570a99e0a43063e74b62ea121ad
Alessandro <Alessandro@LAPTOP-2Q5TD8MO.station>
2021-04-12 09:48:00 +0200

    Soluzione funzionante (che da la prima soluzione valida)

--- src/main/java/it/polito/tdp/regine/model/Regine.java
+++ src/main/java/it/polito/tdp/regine/model/Regine.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.regine.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Regine {
@@ -17,16 +18,70 @@ public class Regine {
 	//     [0, 2]
 	//            [0, 2, 1]
 	
-	/*private void cerca(List<Integer>parziale, int livello) {
+	private int N;
+	private List<Integer> soluzione;
+	public List<Integer> risolvi (int N){
+		this.N=N;
+		
+		List<Integer> parziale = new ArrayList<Integer>();
+		this.soluzione=null;
+		cerca(parziale, 0);
+		return this.soluzione;
+	}
+	
+	// cerca = true : trovato; cerca = false : cerca ancora
+	private boolean cerca(List<Integer> parziale, int livello) {
 		if(livello==N) {
 			// caso terminale
+//			System.out.println(parziale);
+			this.soluzione= new ArrayList<>(parziale);
+			return true;
 		} else {
 			for(int colonna=0; colonna<N; colonna++) {
 				// if la mossa nella casella [livello][colonna] è valida
 				// se sì, aggiungi a parziale e fai ricorsione
+				
+				if(posValida(parziale, colonna)) {
+					
+					// versione senza backtracking (sconsigliabile perché deve creare a ogni iterazione devo creare una lista nuova)
+					// NB negli anagrammi andava bene perché si lavorava su una stringa, le stringhe sono immutabili invece le liste no
+//					List<Integer> parzialeNuovo = new ArrayList<>(parziale);
+//					parzialeNuovo.add(colonna);
+//					cerca(parzialeNuovo, livello+1);
+					
+					// versione con backtracking
+					parziale.add(colonna);
+					boolean trovato=cerca(parziale, livello +1);
+					if(trovato) {
+						return true;
+					}
+					parziale.remove(parziale.size()-1); //backtracking  (così quando risalgo di livello tolgo l'ultimo aggiunto per poterne provare un altro)
+				}
 			}
 		}
-	}*/
+		return false;
+	}
+
+
+	private boolean posValida(List<Integer> parziale, int colonna) {
+		int livello=parziale.size();
+		// controllo riga non serve per come abbiamo strutturato la soluzione
+		// controlla se viene mangiata in verticale
+		if(parziale.contains(colonna)) {
+			return false;
+		}
+		
+		// controlla se viene mangiata in diagonale: confronta la posizione(livello, colonna) con (r,c) delle regine esistenti
+		for(int r=0; r<livello; r++) {
+			int c = parziale.get(r);
+			
+			if(r+c==livello+colonna || r-c==livello-colonna) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 	
 	
 	
